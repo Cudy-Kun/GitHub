8708957bd096b870038be7ff517ce81909725fde
thomp3al <thomp3al@IL-PC-152.uccc.uc.edu>
2018-01-23 18:43:45 -0500

    New project first commit

---
+++ GitHub/src/gitHub/Git.java
@@ -0,0 +1,30 @@
+/*****************************
+ * Name : Abigail Thompson
+ * Assignment #: Assignment GitHub
+ * Course #, Section #: IT2045C Section 1
+ * Due Date:
+ * Description: GitHUb
+ * Citation:
+ * Anything else:
+ */
+package gitHub;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I LOVE OCTOCAT!!");
+		System.out.println("I LOVE OCTOCAT!!!");
+		System.out.println("I LOVE OCTOCAT!!!!");
+		System.out.println("I LOVE OCTOCAT!!!!!");
+		System.out.println("I LOVE OCTOCAT!!!!!!");
+		System.out.println("I LOVE OCTOCAT!!!!!!!");
+		System.out.println("I LOVE OCTOCAT!!!!!!!!");
+		System.out.println("Meow Meow");
+		System.out.println("I am soooo hunger.");
+
+
+
+	}
+
+}
